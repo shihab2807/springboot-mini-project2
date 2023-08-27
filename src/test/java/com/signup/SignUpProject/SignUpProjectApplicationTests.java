2376834f923338544ef5ab715124712b1040e06f
unknown <shihabshiya768@gmail.com>
2023-08-27 12:29:18 +0530

    first commit

---
+++ src/test/java/com/signup/SignUpProject/SignUpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signup.SignUpProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
